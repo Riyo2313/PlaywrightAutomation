a780a3181525efc6caf692835d9aa4051437dcfe
unknown <kbyogita12@gmail.com>
2025-09-28 15:03:22 +0530

    Initial commit

--- tests/InvalidBlan.spec.ts
+++ tests/InvalidBlan.spec.ts
@@ -27,12 +27,13 @@ await page.locator('body').click();
  await page.locator('//input[@id="loginEmail"]').press('Tab');
   // Fill in the password field
   await page.locator('input#loginPassword').fill('');
-  await page.screenshot({ path: 'screenshot.png' });
+  await page.screenshot({ path: 'BlankInput.png' });
   // Click the Login button
   
   await page.getByRole('button', { name: 'Login' }).click();
    await page.waitForTimeout(2000);
-    await page.screenshot({ path: 'screenshot.png' });
+   await page.screenshot({ path: 'AfterBlankInput.png' });
+  // Click the Login button
    const blank= page.locator('p#loginError.error')
   await expect.soft(blank).toHaveText('Invalid email or password.',{timeout:2000})
 });
@@ -40,18 +41,18 @@ await page.locator('body').click();
  test('user login form incorrect credential', async ({ page }) => {
 await page.locator('body').click();
   await page.goto('file:///C:/Users/Rishi\'s%20HP/Downloads/SmartBank.html#');
-  await page.getByRole('textbox', { name: 'Full Name' }).click();
+ 
   await page.getByRole('textbox', { name: 'Full Name' }).fill('Janhavi');
   await page.getByRole('textbox', { name: 'Email' }).fill('janhavi13@gmail.com');
   await page.getByRole('textbox', { name: 'Password', exact: true }).fill('Janhavik');
   await page.getByRole('textbox', { name: 'Confirm Password' }).click();
   await page.getByRole('textbox', { name: 'Confirm Password' }).fill('Janhavik');
-  await page.screenshot({ path: 'screenshot.png' });
+  await page.screenshot({ path: 'sceenshotbeforeg.png' });
   await page.getByRole('button', { name: 'Register' }).click();
   //do not have element 'text' for success message
   //const message= page.locator('//p[@id="regSuccess"]')
   //await expect.soft(message).toHaveText('registration succesful',{timeout:2000})
-
+await page.screenshot({ path: 'sceenshoafterreg.png' });
   // Wait for page to load
 
   await page.getByText('Login here',{exact:true}).click();
@@ -60,68 +61,20 @@ await page.locator('body').click();
   // Optional: Wait extra time for dynamic elements to load
   await page.waitForTimeout(2000);
 
-  await page.locator('//input[@id="loginEmail"]').fill('anhav');
+  await page.locator('//input[@id="loginEmail"]').fill('anhav@gmail.com');
  await page.locator('//input[@id="loginEmail"]').press('Tab');
   // Fill in the password field
   await page.locator('input#loginPassword').fill('Janhav');
-  await page.screenshot({ path: 'screenshot.png' });
+  await page.screenshot({ path: 'Blankinput.png' });
   // Click the Login button
   
   await page.getByRole('button', { name: 'Login' }).click();
    await page.waitForTimeout(2000);
-    await page.screenshot({ path: 'screenshot.png', });
+    await page.screenshot({ path: 'AfterBlank.png', });
    const blank= page.locator('p#loginError.error')
   await expect.soft(blank).toHaveText('Invalid email or password.',{timeout:2000})
 
-
-
-
-
-  // Optional: Wait for navigation/dashboard/etc.
-  /*await page.waitForTimeout(2000); // Adjust as needed
-
-  // Screenshot after login click
-  await page.screenshot({ path: 'after-login-click.png' });
-   await page.waitForTimeout(2000);
-//const welcome= page.locator('//h2[text()="Welcome "]')
-//await expect.soft(welcome).toHaveText("Welcome")
-const usrname= page.locator('span#userName')
-await expect.soft(usrname).toHaveText('userName',{timeout:2000})
-
-//await expect(welcome).toBeVisible();')
-//await expect(welcome).toBeVisible();
-await page.screenshot({ path: 'screenshot.png' });
-
-await page.locator('input#depositAmount').fill('2000')
-await page.locator('//button[@onclick="deposit()"] ').click();
- await page.waitForTimeout(2000);
-
-const confmesg= page.locator('p#depositMsg.success')
-await expect(confmesg).toHaveText('Deposit successful.', {timeout:2000})
-//await page.locator('input#withdrawAmount').fill('700')
-//await page.locator('//button[@onclick="withdraw()"]').click
-//loop throgh
-// wrong arithmatic calculations for withdraw amount,it behaves like it accepting -amount but 
-// when we did it manually it going correctly
-const withdrawAmounts=['700', '-95', '1400']
-for (const amount of withdrawAmounts) {
-    // Fill the withdraw input with current amount
-    await page.locator('#withdrawAmount').fill(amount);
-
-    // Click the withdraw submit button
-    await page.locator('//button[@onclick="withdraw()"]').click();
-await page.screenshot({ path: 'screenshot.png' });
-    // Optional wait between withdrawals
-    await page.waitForTimeout(1000);
-
-}
-await page.screenshot({ path: 'screenshot.png' });
- await page.waitForTimeout(1000);
-const withdrawmsg= page.locator('p#withdrawMsg.success')
-  await expect(withdrawmsg).toHaveText('Withdrawal successful.',{timeout:3000})
-  await page.screenshot({ path: 'screenshot.png' });
- await page.locator('input#withdrawAmount').fill('1400')
-await page.locator('//button[@onclick="withdraw()"]').click*/
+  
 });
 
 test('user login form not registerd email credential', async ({ page }) => {
@@ -151,12 +104,13 @@ await page.locator('body').click();
  await page.locator('//input[@id="loginEmail"]').press('Tab');
   // Fill in the password field
   await page.locator('input#loginPassword').fill('Khushibk');
-  await page.screenshot({ path: 'screenshot.png' });
+   await page.screenshot({ path: 'Incorrectscreenshot.png' });
   // Click the Login button
   
   await page.getByRole('button', { name: 'Login' }).click();
    await page.waitForTimeout(2000);
-    await page.screenshot({ path: 'screenshot.png', });
+   await page.screenshot({ path: 'AfterIncorrectscreenshot.png' });
+      
    const blank= page.locator('p#loginError.error')
   await expect.soft(blank).toHaveText('Invalid email or password.',{timeout:2000})
 
